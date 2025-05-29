8a5fe10652dc85790a566cf1f84d9e251571f8c2
dentra <dennis.trachuk@gmail.com>
2025-05-29 21:52:29 +0300

    add tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,15 +6,21 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'dart:async';
+import 'dart:io';
 import 'dart:typed_data';
 
 // import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:convert/convert.dart';
 import 'package:logger/logger.dart';
+import 'package:tion_web/firmware.dart';
 
 import 'package:tion_web/tion.dart';
 import 'package:tion_web/log.dart';
+
+void logError(String message) {
+  throw Exception(message);
+}
 
 void main() {
   test('create update start request', () async {
@@ -33,6 +39,26 @@ void main() {
     });
 
     await tion.tx(startReq);
+  });
+
+  test('list fw', () async {
+    final list = await FirmwareType.br4S.list(logError);
+    expect(list.length, 4);
+  });
+
+  test('sha1 webcrypto', () async {
+    final list = await FirmwareType.unknown.list(logError);
+    await Future.forEach(list, (fwInfo) async {
+      log.i("Processing ${fwInfo.type.type} ${fwInfo.name}");
+      // final fwFile = File(
+      //     "../tion-firmware-gh/firmware_${fwInfo.type.type}_${fwInfo.name}.bin");
+      // log.d("Processing $fwFile");
+      // expect(fwFile.existsSync(), true);
+      // final fwData = await fwFile.readAsBytes();
+      final fwData = await fwInfo.load(logError);
+      final result = await fwInfo.validate(fwData, logError);
+      expect(result, true);
+    });
   });
 
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
