5ab03086d37d06a708fec570f06ebe6a0bfdc5dd
dentra <dennis.trachuk@gmail.com>
2025-05-30 18:33:45 +0300

    add app revision display

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ import 'tion.dart';
 import 'state_widget.dart';
 import 'log.dart';
 
+const _appRevision =
+    String.fromEnvironment('APP_REVISION', defaultValue: "dev");
+
 void main() async {
   initLog();
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,7 +30,6 @@ class _TionFlasherAppState extends State<TionFlasherApp>
   late TabController _tabController;
 
   BluetoothDevice? _device;
-  BuildContext? _floatContext;
 
   final tion = TionBLE();
   final updateStateController = UpdateStateController();
@@ -65,112 +67,127 @@ class _TionFlasherAppState extends State<TionFlasherApp>
       home: DefaultTabController(
         length: tion.connected ? 2 : 1,
         child: Scaffold(
-          appBar: AppBar(
-            // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: Text(tion.connected ? "$title: ${tion.devName}" : title),
-            bottom: TabBar(
-              controller: tion.connected ? _tabController : null,
-              tabs: [
-                if (!tion.connected)
-                  const Tab(
-                    icon: Icon(Icons.info_outline),
-                    text: "Подключение",
-                  ),
-                if (tion.connected &&
-                    updateStateController.value == UpdateState.stopped)
-                  const Tab(
-                    icon: Icon(Icons.hvac_outlined),
-                    text: "Состояние",
-                  ),
-                if (tion.connected)
-                  const Tab(
-                    icon: Icon(Icons.system_update_alt_outlined),
-                    text: "Прошивка",
-                  ),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            controller: tion.connected ? _tabController : null,
+          appBar: appBar(title),
+          body: Column(
             children: [
-              if (!tion.connected && !isBluetoothApiSupported)
-                const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.warning_outlined,
-                          color: Colors.amber,
-                        ),
-                        Text(
-                            'К сожалению, Ваш браузер не поддерживает Bluetooth'),
-                      ],
-                    ),
-                  ],
-                ),
-              if (!tion.connected && isBluetoothApiSupported)
-                const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text('Ожидание соединения...'),
-                    Padding(padding: EdgeInsets.symmetric(vertical: 8.0)),
-                    Text(
-                        'Для подключения используйте плавающую кнопку справа внизу.'),
-                    Padding(padding: EdgeInsets.symmetric(vertical: 8.0)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.warning_outlined,
-                          color: Colors.amber,
-                        ),
-                        Text(
-                            'При первичном подключении Вам необходимо ввести бризер в режим сопряжения.'),
-                      ],
-                    ),
-                  ],
-                ),
-              if (tion.connected)
-                TionStateWidget(
-                  tion: tion,
-                ),
-              if (tion.connected)
-                TionFirmwareWidget(
-                  tion: tion,
-                  updateStateController: updateStateController,
-                ),
+              Expanded(
+                child: tabBarView(),
+              ),
+              Text("Ревизия: $_appRevision")
             ],
           ),
           floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-          floatingActionButton: !isBluetoothApiSupported
-              ? null
-              : Builder(builder: (context) {
-                  return FloatingActionButton(
-                    onPressed: _requestDevice(context),
-                    tooltip:
-                        tion.connected ? 'Переподключиться' : 'Подключиться',
-                    child: Icon(tion.connected
-                        ? Icons.bluetooth_connected_outlined
-                        : Icons.bluetooth_outlined),
-                  );
-                }),
+          floatingActionButton: floatingActionButton(),
         ),
       ),
     );
   }
 
-  VoidCallback _requestDevice(BuildContext context) {
-    _floatContext = context;
-    return requestDevice;
+  Widget? floatingActionButton() {
+    if (!isBluetoothApiSupported) {
+      return null;
+    }
+    return Builder(builder: (context) {
+      return FloatingActionButton(
+        onPressed: () {
+          requestDevice(context);
+        },
+        tooltip: tion.connected ? 'Переподключиться' : 'Подключиться',
+        child: Icon(tion.connected
+            ? Icons.bluetooth_connected_outlined
+            : Icons.bluetooth_outlined),
+      );
+    });
   }
 
-  Future<void> requestDevice() async {
+  TabBarView tabBarView() {
+    return TabBarView(
+      controller: tion.connected ? _tabController : null,
+      children: [
+        if (!tion.connected && !isBluetoothApiSupported)
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.warning_outlined,
+                    color: Colors.amber,
+                  ),
+                  Text('К сожалению, Ваш браузер не поддерживает Bluetooth'),
+                ],
+              ),
+            ],
+          ),
+        if (!tion.connected && isBluetoothApiSupported)
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Ожидание соединения...'),
+              Padding(padding: EdgeInsets.symmetric(vertical: 8.0)),
+              Text(
+                  'Для подключения используйте плавающую кнопку справа внизу.'),
+              Padding(padding: EdgeInsets.symmetric(vertical: 8.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.warning_outlined,
+                    color: Colors.amber,
+                  ),
+                  Text(
+                      'При первичном подключении Вам необходимо ввести бризер в режим сопряжения.'),
+                ],
+              ),
+            ],
+          ),
+        if (tion.connected)
+          TionStateWidget(
+            tion: tion,
+          ),
+        if (tion.connected)
+          TionFirmwareWidget(
+            tion: tion,
+            updateStateController: updateStateController,
+          ),
+      ],
+    );
+  }
+
+  AppBar appBar(String title) {
+    return AppBar(
+      // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: Text(tion.connected ? "$title: ${tion.devName}" : title),
+      bottom: TabBar(
+        controller: tion.connected ? _tabController : null,
+        tabs: [
+          if (!tion.connected)
+            const Tab(
+              icon: Icon(Icons.info_outline),
+              text: "Подключение",
+            ),
+          if (tion.connected &&
+              updateStateController.value == UpdateState.stopped)
+            const Tab(
+              icon: Icon(Icons.hvac_outlined),
+              text: "Состояние",
+            ),
+          if (tion.connected)
+            const Tab(
+              icon: Icon(Icons.system_update_alt_outlined),
+              text: "Прошивка",
+            ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> requestDevice(BuildContext context) async {
     if (!isBluetoothApiSupported) {
-      _reportError("Отсутствует поддержка Bluetooth в браузере");
+      _reportMessage(context, "Отсутствует поддержка Bluetooth в браузере");
       return;
     }
 
@@ -210,27 +227,34 @@ class _TionFlasherAppState extends State<TionFlasherApp>
 
       await _device!.connect();
       log.i("Device ${_device!.name ?? ""} connected");
-      _reportError("Подключено");
+      _reportMessage(
+          context, // ignore: use_build_context_synchronously
+          "Подключено");
     } on BluetoothAdapterNotAvailable {
-      _reportError("Отсутствует Bluetooth-адаптер");
+      _reportMessage(
+          context, // ignore: use_build_context_synchronously
+          "Отсутствует Bluetooth-адаптер");
     } on UserCancelledDialogError {
-      _reportError("Подключение отменено");
+      _reportMessage(
+          context, // ignore: use_build_context_synchronously
+          "Подключение отменено");
     } on DeviceNotFoundError {
-      _reportError("Не найдено подходящих устройств");
+      _reportMessage(
+          context, // ignore: use_build_context_synchronously
+          "Не найдено подходящих устройств");
     } catch (e, s) {
-      _reportError("Возможно не обеспечено сопряжение с бризером!\n$e");
+      _reportMessage(
+          context, // ignore: use_build_context_synchronously
+          "Возможно не обеспечено сопряжение с бризером!\n$e");
       log.e(e, stackTrace: s);
     }
   }
 
-  void _reportError(final String message) {
-    if (_floatContext == null) {
+  void _reportMessage(BuildContext context, final String message) {
+    if (!context.mounted) {
       return;
     }
-    if (!_floatContext!.mounted) {
-      return;
-    }
-    ScaffoldMessenger.of(_floatContext!).showSnackBar(
+    ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(message),
       ),
